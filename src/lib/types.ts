e8ba8d10404cc7261ead4a061293a8eca8d894e1
tkachuk <z.tkachuk@nodeart.io>
2024-06-23 19:23:27 +0300

    optimization. part I

---
+++ src/lib/types.ts
@@ -0,0 +1,17 @@
+export interface Symbol {
+    d: string;
+}
+
+export interface Link {
+    [key: number]: number;
+}
+
+export type Chain = number[];
+
+export interface MatrixUnit {
+    chain: Chain;
+    speed: number;
+    y: number;
+}
+
+export type Matrix = MatrixUnit[];
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	// config
 	import {
 		symbols,
 		bgColor,
@@ -16,7 +17,12 @@
 		maxWaterfallSpeed,
 		symbolShuffleSpeed
 	} from '$lib/config';
+	// types
+	import type { Symbol, Chain, Matrix } from '$lib/types';
+	// components
 	import FPS from '$lib/fps.svelte';
+	import Config from '$lib/config.svelte';
+	// styles
 	import '../app.scss';
 
 	let fpsOpen = false;
@@ -25,7 +31,8 @@
 	let matrixHeight = 0;
 	let canvas: HTMLCanvasElement;
 	let ctx: CanvasRenderingContext2D | null;
-	let matrix: any[] = [];
+	let pathCache: any = {};
+	let matrix: Matrix;
 
 	const getY = () => -Math.random() * matrixHeight; //  * 0.5
 	const getSpeed = () => Math.random() / getRondomNumner(minWaterfallSpeed, maxWaterfallSpeed);
@@ -43,24 +50,32 @@
 		draw();
 	}
 
+	const getOrCreatePath = (symbol: Symbol) => {
+		if (!pathCache[symbol.d]) {
+			pathCache[symbol.d] = new Path2D(symbol.d);
+		}
+		return pathCache[symbol.d];
+	};
+
 	function draw() {
 		if (!ctx || !canvas) return;
 		ctx.fillStyle = bgColor;
 		ctx.fillRect(0, 0, matrixWidth, matrixHeight);
-		ctx.fillStyle = matrixColor;
 
 		for (let i = 0; i < matrix.length; i++) {
-			let chain = matrix[i].chain;
+			let chain: Chain = matrix[i].chain;
 
 			for (let j = 0; j < chain.length; j++) {
 				const isFirstSymbol = j === chain.length - 1;
-				let symbol =
+				let symbol: Symbol =
 					isFirstSymbol || (!isFirstSymbol && Math.random() * 100 > 100 - j * symbolShuffleSpeed)
 						? getRondomSymbol()
 						: symbols[chain[j]];
-				let path = new Path2D(symbol.d);
+
+				let path = getOrCreatePath(symbol);
 
 				ctx.save();
+				ctx.textRendering = 'optimizeSpeed';
 				ctx.translate(i * 10, matrix[i].y + j * (symbolSize + 15));
 				ctx.scale(symbolScale, symbolScale);
 				ctx.fillStyle = isFirstSymbol ? firstSymbolColor : matrixColor;
@@ -69,7 +84,6 @@
 				ctx.globalAlpha = 0.02 + (1 - 0.2) * (j / (chain.length - 1));
 				ctx.fill(path);
 				ctx.restore();
-
 				matrix[i].y += matrix[i].speed;
 				if (matrix[i].y > canvas.height) matrix[i] = getNewMatrix();
 			}
@@ -89,9 +103,9 @@
 	});
 </script>
 
-<svelte:window bind:innerWidth={matrixWidth} bind:innerHeight={matrixHeight} on:keyup={keyup} />
+<svelte:window on:keyup={keyup} bind:innerWidth={matrixWidth} bind:innerHeight={matrixHeight} />
 {#if fpsOpen}<FPS />{/if}
-{#if configModalOpen}config{/if}
+{#if configModalOpen}<Config />{/if}
 <canvas
 	bind:this={canvas}
 	width={matrixWidth}
