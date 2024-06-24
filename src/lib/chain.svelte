e90413d8e907c3d842c6d9b82bd458cb61080195
tkachuk <z.tkachuk@nodeart.io>
2024-06-24 22:50:36 +0300

    update

--- src/lib/chain.svelte
+++ src/lib/chain.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
 	// config
-	import { speed, symbols, symbolSize, symbolsCount, getRondomNumner } from '$lib/config';
+	import {
+		speed,
+		symbols,
+		maxDelay,
+		symbolSize,
+		symbolsCount,
+		minChainSize,
+		maxChainSize,
+		getRondomNumner,
+	} from '$lib/config';
 	import Symbol from '$lib/symbol.svelte';
 	// styles
 	import '../app.scss';
 
 	export let matrixHeight: number;
-	const chainLength = Math.ceil(matrixHeight / (symbolSize * 10));
+	const chainLength = getRondomNumner(minChainSize, maxChainSize);
 	const chain = new Array(chainLength)
 		.fill(null)
 		.map(() => symbols[getRondomNumner(1, symbolsCount - 1)]);
 </script>
 
-<div class="chain" style="--speed: {speed}ms; --delay: {getRondomNumner(1, 2500)}ms;">
+<div class="chain" style="--speed: {speed}ms; --delay: {getRondomNumner(1, maxDelay)}ms;">
 	{#each chain as _, index}
 		<Symbol {index} chainLength={chain.length - 1} />
 	{/each}
@@ -26,9 +35,7 @@
 		flex-flow: column wrap;
 		align-items: flex-start;
 		justify-content: flex-start;
-
 		width: 100%;
-		animation:
-			var(--speed) linear var(--delay) infinite running waterfall;
+		animation: var(--speed) linear var(--delay) infinite running waterfall;
 	}
 </style>
--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -14,12 +14,13 @@ const symbols = [
 
 const symbolsCount = symbols.length;
 const matrixColor = '#ADF69C';
-const shadowColor = '#068029';
+const shadowColor = '#06802970';
 const firstSymbolColor = '#ACFFD7';
 const symbolSize = 3; // 1.6
 const speed = 18 * 1000;
-const minChainSize = 80; // percentage of the screen
-const maxChainSize = 120; // percentage of the screen
+const maxDelay = 5000;
+const minChainSize = 30; // percentage of the screen
+const maxChainSize = 50; // percentage of the screen
 
 const getRondomNumner = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min) + min);
@@ -28,6 +29,7 @@ const getRondomNumner = (min: number, max: number) => {
 export {
     speed,
     symbols,
+    maxDelay,
     symbolSize,
     matrixColor,
     shadowColor,
