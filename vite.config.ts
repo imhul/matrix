5ac0eee75eb9641b1de387ddc6fa18666117407c
tkachuk <z.tkachuk@nodeart.io>
2025-12-28 00:01:20 +0100

    build

--- vite.config.ts
+++ vite.config.ts
@@ -2,39 +2,39 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: './',
-    server: {
-        port: 8080,
-        open: true
-    },
-    build: {
-        rollupOptions: {
-            output: {
-                entryFileNames: 'matrix-rain.js',
-                assetFileNames: 'matrix-rain.[ext]',
-                inlineDynamicImports: true
-            }
-        },
-        cssCodeSplit: false,
-        minify: 'terser',
-        terserOptions: {
-            compress: {
-                drop_console: true,
-                drop_debugger: true,
-                pure_funcs: ['console.log', 'console.info', 'console.debug'],
-                passes: 4
-            },
-            mangle: {
-                toplevel: true,
-                properties: {
-                    regex: /^_/
-                }
-            },
-            format: {
-                comments: false
-            }
-        },
-        target: 'es2020',
-        modulePreload: false
-    }
+	base: './',
+	server: {
+		port: 8080,
+		open: true
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: 'matrix-rain.js',
+				assetFileNames: 'matrix-rain.[ext]',
+				inlineDynamicImports: true
+			}
+		},
+		cssCodeSplit: false,
+		minify: 'terser',
+		terserOptions: {
+			compress: {
+				drop_console: true,
+				drop_debugger: true,
+				pure_funcs: ['console.log', 'console.info', 'console.debug'],
+				passes: 4
+			},
+			mangle: {
+				toplevel: true,
+				properties: {
+					regex: /^_/
+				}
+			},
+			format: {
+				comments: false
+			}
+		},
+		target: 'es2020',
+		modulePreload: false
+	}
 })
