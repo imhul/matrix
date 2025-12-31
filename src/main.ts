fcf9ebb6edda8e959f6d3146490fefe723e911e4
tkachuk <z.tkachuk@nodeart.io>
2025-12-31 19:31:31 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -54,6 +54,14 @@ async function showWelcomeScreen(): Promise<void> {
 
 // app
 ;(async () => {
+	await new Promise<void>((resolve) => {
+		if (document.readyState === 'loading') {
+			document.addEventListener('DOMContentLoaded', () => resolve())
+		} else {
+			resolve()
+		}
+	})
+
 	const app = new Application()
 	await app.init(appConfig)
 	await showWelcomeScreen()
